869768dd8925be0ea24d41c1892b341cd27baacb
Chris Park <cpark@basistech.com>
2016-01-27 15:42:12 -0500

    Fixed base64 example

--- examples/base64_input.sh
+++ examples/base64_input.sh
@@ -6,8 +6,10 @@ if [ $# -eq 0 ]; then
 fi
 entities_text_data="Bill Murray will appear in new Ghostbusters film: Dr. Peter Venkman was spotted filming a cameo in Boston this… http://dlvr.it/BnsFfS"
 
+encoded_data=`echo "$entities_text_data" | base64 -w0`
+
 curl "https://api.rosette.com/rest/v1/entities" \
   -H "user_key: $1" \
   -H 'Content-Type:application/json' \
   -H 'Accept:application/json' \
-  -d "{\"content\": \"$entities_text_data | base64\"}"
+  -d "{\"content\":\"$encoded_data\",\"contentType\":\"application/octet-stream\"}"
