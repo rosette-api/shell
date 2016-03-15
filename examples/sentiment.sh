77c4a005924e81c6f20f0aec3ad08fd670156637
Chris Park <cpark@basistech.com>
2016-03-15 13:00:23 -0400

    Modified sentiment to use multipart, removed base64

--- examples/sentiment.sh
+++ examples/sentiment.sh
@@ -6,14 +6,18 @@ if [ $# -lt 1 ]; then
 fi
 sentiment_text_data="Original Ghostbuster Dan Aykroyd, who also co-wrote the 1984 Ghostbusters film, couldn’t be more pleased with the new all-female Ghostbusters cast, telling The Hollywood Reporter, “The Aykroyd family is delighted by this inheritance of the Ghostbusters torch by these most magnificent women in comedy.”"
 
+TMPFILE=`mktemp` || exit 1
+echo ${sentiment_text_data} >> ${TMPFILE}
+
 if [ -z "$2" ]; then
     url="https://api.rosette.com/rest/v1/sentiment"
 else
     url="$2/sentiment"
 fi
 
-curl "$url" \
+curl \
+  -F "content=@${TMPFILE}" \
+  -F "request= {\"language\": \"eng\"};type=application/json" \
   -H "X-RosetteAPI-Key: $1" \
-  -H 'Content-Type:application/json' \
   -H 'Accept:application/json' \
-  -d "{\"content\": \"$sentiment_text_data\", \"language\": \"eng\", \"options\": {\"model\": \"short-string\", \"explain\": true}}"
+   "$url" \
