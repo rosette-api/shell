838bfe791de2c91e25f4a5f6056ece36ac82454a
Chris Park <cpark@basistech.com>
2016-03-31 18:23:55 +0000

    Replaced sentiment_text_data with sentiment_file_data

--- examples/sentiment.sh
+++ examples/sentiment.sh
@@ -4,10 +4,10 @@ if [ $# -lt 1 ]; then
     echo "usage: $0 api_key [alternate_url]"
     exit 1
 fi
-sentiment_text_data="Original Ghostbuster Dan Aykroyd, who also co-wrote the 1984 Ghostbusters film, couldn’t be more pleased with the new all-female Ghostbusters cast, telling The Hollywood Reporter, “The Aykroyd family is delighted by this inheritance of the Ghostbusters torch by these most magnificent women in comedy.”"
+sentiment_file_data="<html><head><title>New Ghostbusters Film</title></head><body><p>Original Ghostbuster Dan Aykroyd, who also co-wrote the 1984 Ghostbusters film, couldn’t be more pleased with the new all-female Ghostbusters cast, telling The Hollywood Reporter, The Aykroyd family is delighted by this inheritance of the Ghostbusters torch by these most magnificent women in comedy.</p></body></html>"
 
 TMPFILE=`mktemp` || exit 1
-echo ${sentiment_text_data} >> ${TMPFILE}
+echo ${sentiment_file_data} >> ${TMPFILE}
 
 if [ -z "$2" ]; then
     url="https://api.rosette.com/rest/v1/sentiment"
