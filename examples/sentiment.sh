b8216c58bcc3cfc0a3af9952f4d3255d920d50d5
Chris Park <cpark@basistech.com>
2016-04-26 13:08:43 -0400

    Sentiment example update

--- examples/sentiment.sh
+++ examples/sentiment.sh
@@ -4,7 +4,7 @@ if [ $# -lt 1 ]; then
     echo "usage: $0 api_key [alternate_url]"
     exit 1
 fi
-sentiment_file_data="<html><head><title>New Ghostbusters Film</title></head><body><p>Original Ghostbuster Dan Aykroyd, who also co-wrote the 1984 Ghostbusters film, couldn’t be more pleased with the new all-female Ghostbusters cast, telling The Hollywood Reporter, The Aykroyd family is delighted by this inheritance of the Ghostbusters torch by these most magnificent women in comedy.</p></body></html>"
+sentiment_file_data="<html><head><title>New Ghostbusters Film</title></head><body><p>Original Ghostbuster Dan Aykroyd, who also co-wrote the 1984 Ghostbusters film, couldn’t be more pleased with the new all-female Ghostbusters cast, telling The Hollywood Reporter, “The Aykroyd family is delighted by this inheritance of the Ghostbusters torch by these most magnificent women in comedy.”</p></body></html>"
 
 TMPFILE=`mktemp` || exit 1
 echo ${sentiment_file_data} >> ${TMPFILE}
