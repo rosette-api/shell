65856cc61d3c475a68619b290216609954dad5f4
kbailey-basistech <kbailey@basistech.com>
2016-09-26 16:19:29 -0400

    syntax dependencies added

---
+++ examples/syntax_dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+if [ $# -lt 1 ]; then
+    echo "usage: $0 api_key [alternate_url]"
+    exit 1
+fi
+syntax_dependencies_data="Sony Pictures is planning to shoot a good portion of the new Ghostbusters in Boston as well."
+
+if [ -z "$2" ]; then
+    url="https://api.rosette.com/rest/v1/syntax/dependencies"
+else
+    url="$2/syntax/dependencies"
+fi
+
+curl -s "$url" \
+  -H "X-RosetteAPI-Key: $1" \
+  -H 'Content-Type:application/json' \
+  -H 'Accept:application/json' \
+  -d "{\"content\": \"$syntax_dependencies_data\"}"
